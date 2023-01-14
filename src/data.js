505d4f08acee987686244dd5d9eb28af0021238b
Adam Yu <chiyehyu@gmail.com>
2023-01-14 09:10:55 +0800

    change data texts

--- src/data.js
+++ src/data.js
@@ -6,8 +6,8 @@ import tour4Img from './images/tour-4.jpeg';
 export const pageLinks = [
   { id: 1, href: '#home', text: 'Home' },
   { id: 2, href: '#about', text: 'About' },
-  { id: 3, href: '#services', text: 'Services' },
-  { id: 4, href: '#tours', text: 'Tours' },
+  { id: 3, href: '#services', text: 'Service' },
+  { id: 4, href: '#tours', text: 'Tour' },
 ];
 
 export const socialLinks = [
